2bdd78a7e147ebcd29cb3dcd1416e97c415f1d63
husseinfaraj7 <ussofaraj@gmail.com>
2025-10-20 23:52:59 +0200

    Delete email-config.js

--- email-config.js
+++
@@ -1,8 +0,0 @@
-const EMAIL_CONFIG = {
-    backend: {
-        url: 'http://localhost:3000',
-        healthEndpoint: '/health',
-        sendEndpoint: '/send-email'
-    },
-    timeout: 10000
-};
